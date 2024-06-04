433faf85d5ec085a4d744bb00bedd8785f2428aa
hulk50 <biplab.m@utahtechlab.com>
2024-06-04 15:20:31 +0530

    fibonacci

---
+++ fibonacci/index.js
@@ -0,0 +1,32 @@
+// fib series is  [0,1,1,2,3,5,8];
+
+const fiboNacci = (n) =>{
+            let fib =[0,1];
+        for(let  i=2; i<n ; i++){
+           
+             fib[i] = fib[i-1] +fib[i-2]
+         
+
+        }
+
+        return fib;
+}
+
+const  recursiveFobonacci = (n) =>{
+
+            if(n<2){
+                return n;
+            }
+                                              
+            return recursiveFobonacci(n-1) +  recursiveFobonacci(n-2)
+
+}
+
+
+console.log(recursiveFobonacci(3));
+
+
+
+
+
+ 
